b739905428c8a9ca6ddb30d2ebd68e46ed6ac293
BobbleLaw <bobble2579@hotmail.com>
2025-07-26 20:58:33 +0100

    refactor: cleanup iKdTree
    + Remove template
    Internal data only requires the point type can convert to a Vector3f
    + Use Eigen::AlignedBox for AABB
    + Use Google style
    + const everything possible
    + Reorganize some logic
    + Use atomic type for mutex protected boolean

--- modules/space/src/ikdtree.h
+++ modules/space/src/ikdtree.h
@@ -1,44 +1,12 @@
 #pragma once
 
-#include <cmath>
-#include <memory>
 #include <vector>
 #include <mutex>
 #include <thread>
 
-#define EPSS 1e-6
-#define Minimal_Unbalanced_Tree_Size 10
-#define Multi_Thread_Rebuild_Point_Num 1500
-#define DOWNSAMPLE_SWITCH true
-#define ForceRebuildPercentage 0.2
+#include <Eigen/Geometry>
+
 #define Q_LEN 1000000
-
-struct ikdTree_PointType
-{
-    float x, y, z;
-    ikdTree_PointType(float px = 0.0f, float py = 0.0f, float pz = 0.0f)
-    {
-        x = px;
-        y = py;
-        z = pz;
-    }
-};
-
-struct BoxPointType
-{
-    float vertex_min[3];
-    float vertex_max[3];
-};
-
-enum operation_set
-{
-    ADD_POINT,
-    DELETE_POINT,
-    DELETE_BOX,
-    ADD_BOX,
-    DOWNSAMPLE_DELETE,
-    PUSH_DOWN
-};
 
 enum delete_point_storage_set
 {
@@ -50,32 +18,32 @@ enum delete_point_storage_set
 template <typename T>
 class MANUAL_Q
 {
+public:
+    void push(T op);
+    void pop();
+    void clear();
+    T front() const;
+    T back() const;
+    bool empty() const;
+    int size() const;
+
 private:
     int head = 0, tail = 0, counter = 0;
     T q[Q_LEN];
     bool is_empty;
-
-public:
-    void pop();
-    T front() const;
-    T back() const;
-    void clear();
-    void push(T op);
-    bool empty();
-    int size();
 };
 
-template <typename PointType>
+using PointType = Eigen::Vector3f;
+using _PointCloud = std::vector<PointType>;
+
 class iKdTree
 {
 public:
-    using PointVector = std::vector<PointType>;
-
-    struct iKdTreeNode
+    struct Node
     {
         PointType point;
         uint8_t division_axis;
-        int TreeSize = 1;
+        int tree_size = 1;
         int invalid_point_num = 0;
         int down_del_num = 0;
         bool point_deleted = false;
@@ -87,40 +55,61 @@ public:
         bool working_flag = false;
         float radius_sq;
         std::mutex push_down_mutex_lock;
-        float node_range_x[2], node_range_y[2], node_range_z[2];
-        iKdTreeNode *left_son_ptr = nullptr;
-        iKdTreeNode *right_son_ptr = nullptr;
-        iKdTreeNode *father_ptr = nullptr;
+        Eigen::AlignedBox3f range;
+        Node *left = nullptr;
+        Node *right = nullptr;
+        Node *parent = nullptr;
+
         // For paper data record
         float alpha_del;
         float alpha_bal;
+
+        void Init();
+
+        float CalcBoxDistance(const PointType &point) const;
+
+        void Update();
     };
 
-    struct Operation_Logger_Type
+    struct Operation
     {
+        enum Type
+        {
+            kAddPoint,
+            kDeletePoint,
+            kAddBox,
+            kDeleteBox,
+            kDownsampleDelete,
+            kPushDown
+        };
+
         PointType point;
-        BoxPointType boxpoint;
-        bool tree_deleted, tree_downsample_deleted;
-        operation_set op;
+        Eigen::AlignedBox3f box;
+        Type type;
+        bool tree_deleted;
+        bool tree_downsample_deleted;
     };
 
     struct PointType_CMP
     {
         PointType point;
-        float dist = 0.0;
-        PointType_CMP(PointType p = PointType(), float d = INFINITY)
+        float dist = 0.f;
+
+        explicit PointType_CMP(PointType p = PointType(), float d = INFINITY)
+            : point(p), dist(d)
         {
-            this->point = p;
-            this->dist = d;
-        };
+        }
 
         bool operator<(const PointType_CMP &a) const
         {
-            if (fabs(dist - a.dist) < 1e-10)
-                return point.x < a.point.x;
-            else
-                return dist < a.dist;
+            if (std::abs(dist - a.dist) < 1e-10) {
+                return point.x() < a.point.x();
+            }
+
+            return dist < a.dist;
         }
+
+        bool operator>=(const PointType_CMP &a) const { return !(*this < a); }
     };
 
     class MANUAL_HEAP
@@ -144,7 +133,6 @@ public:
             heap[heap_size] = point;
             FloatUp(heap_size);
             heap_size++;
-            return;
         }
 
         void pop()
@@ -208,7 +196,7 @@ public:
     };
 
 public:
-    using Ptr = std::shared_ptr<iKdTree<PointType>>;
+    using Ptr = std::shared_ptr<iKdTree>;
 
     iKdTree(float delete_param = 0.5, float balance_param = 0.6,
             float box_length = 0.2);
@@ -224,81 +212,76 @@ public:
     int size() const;
     int validnum() const;
     void root_alpha(float &alpha_bal, float &alpha_del);
-    void Build(PointVector point_cloud);
-    void Nearest_Search(PointType point, int k_nearest,
-                        PointVector &Nearest_Points,
-                        std::vector<float> &Point_Distance,
-                        double max_dist = INFINITY);
-    void Box_Search(const BoxPointType &Box_of_Point, PointVector &Storage);
-    void Radius_Search(PointType point, float radius, PointVector &Storage);
-    int Add_Points(PointVector &PointToAdd, bool downsample_on);
-    void Add_Point_Boxes(std::vector<BoxPointType> &BoxPoints);
-    void Delete_Points(PointVector &PointToDel);
-    int Delete_Point_Boxes(std::vector<BoxPointType> &BoxPoints);
-    void flatten(iKdTreeNode *root, PointVector &Storage,
-                 delete_point_storage_set storage_type);
-    void acquire_removed_points(PointVector &removed_points);
+    Eigen::AlignedBox3f tree_range() const;
 
-    BoxPointType tree_range() const;
+    void Build(_PointCloud &point_cloud);
+    void NearestSearch(const PointType &point, int kth,
+                       _PointCloud &nearest_points,
+                       std::vector<float> &nearest_distances,
+                       double max_dist = INFINITY);
+    void BoxSearch(const Eigen::AlignedBox3f &box, _PointCloud &points);
+    void RadiusSearch(const PointType &point, float radius,
+                      _PointCloud &points);
+
+    int AddPoints(const _PointCloud &points, bool downsample);
+    void DeletePoints(const _PointCloud &points);
+    void AddBoxes(const std::vector<Eigen::AlignedBox3f> &boxes);
+    int DeleteBoxes(const std::vector<Eigen::AlignedBox3f> &boxes);
+
+    void flatten(Node *root, delete_point_storage_set storage_type,
+                 _PointCloud &Storage);
+    // NOTE: Not const because this function will clear cache points
+    _PointCloud acquire_removed_points();
 
 public:
-    PointVector PCL_Storage;
-    iKdTreeNode *Root_Node = nullptr;
+    _PointCloud PCL_Storage;
+    Node *root_ = nullptr;
     int max_queue_size = 0;
 
 private:
-    // FIXME: Quit using pthread
     static void *multi_thread_ptr(void *arg);
     void multi_thread_rebuild();
     void start_thread();
     void stop_thread();
 
-    void run_operation(iKdTreeNode **root, Operation_Logger_Type operation);
+    void run_operation(Node **root, const Operation &operation);
 
-    void InitTreeNode(iKdTreeNode *root);
-    void Test_Lock_States(iKdTreeNode *root);
-    void BuildTree(iKdTreeNode **root, int l, int r, PointVector &Storage);
-    void Rebuild(iKdTreeNode **root);
-    int Delete_by_range(iKdTreeNode **root, BoxPointType boxpoint,
-                        bool allow_rebuild, bool is_downsample);
-    void Delete_by_point(iKdTreeNode **root, PointType point,
-                         bool allow_rebuild);
-    void Add_by_point(iKdTreeNode **root, PointType point, bool allow_rebuild,
-                      int father_axis);
-    void Add_by_range(iKdTreeNode **root, BoxPointType boxpoint,
-                      bool allow_rebuild);
-    void Search(iKdTreeNode *root, int k_nearest, PointType point,
-                MANUAL_HEAP &q, double max_dist);
-    void Search_by_range(iKdTreeNode *root, BoxPointType boxpoint,
-                         PointVector &Storage);
-    void Search_by_radius(iKdTreeNode *root, PointType point, float radius,
-                          PointVector &Storage);
-    bool Criterion_Check(iKdTreeNode *root);
-    void Push_Down(iKdTreeNode *root);
-    void Update(iKdTreeNode *root);
-    void delete_tree_nodes(iKdTreeNode **root);
-    void downsample(iKdTreeNode **root);
-    bool same_point(PointType a, PointType b);
-    float calc_dist(PointType a, PointType b);
-    float calc_box_dist(iKdTreeNode *node, PointType point);
-    static bool point_cmp_x(PointType a, PointType b);
-    static bool point_cmp_y(PointType a, PointType b);
-    static bool point_cmp_z(PointType a, PointType b);
+    void BuildTree(Node **root, int l, int r, _PointCloud &out_points);
+    void Rebuild(Node **root);
+
+    int DeleteByRange(Node **root, const Eigen::AlignedBox3f &range,
+                      bool rebuild, bool downsample);
+    void DeleteByPoint(Node **root, const PointType &point, bool rebuild);
+    void AddByPoint(Node **root, const PointType &point, bool rebuild,
+                    int father_axis);
+    void AddByRange(Node **root, const Eigen::AlignedBox3f &range,
+                    bool rebuild);
+    void Search(Node *root, int kth, const PointType &point, MANUAL_HEAP &q,
+                double max_dist);
+    void SearchByRange(Node *root, const Eigen::AlignedBox3f &range,
+                       _PointCloud &out_points);
+    void SearchByRadius(Node *root, const PointType &point, float radius,
+                        _PointCloud &out_points);
+
+    bool Criterion_Check(Node *root) const;
+    void Push_Down(Node *root);
+    void Update(Node *root);
+    void delete_tree_nodes(Node **root);
+    void downsample(Node **root);
 
 private:
     // Multi-thread Tree Rebuild
     // TODO: Maybe use atomic
-    bool termination_flag = false;
+    std::atomic<bool> termination_flag = false;
     bool rebuild_flag = false;
     std::thread rebuild_thread;
-    std::mutex termination_flag_mutex_lock, rebuild_ptr_mutex_lock,
-        search_flag_mutex;
+    std::mutex rebuild_ptr_mutex_lock, search_flag_mutex;
     mutable std::mutex working_flag_mutex;
     std::mutex rebuild_logger_mutex_lock, points_deleted_rebuild_mutex_lock;
-    // queue<Operation_Logger_Type> Rebuild_Logger;
-    MANUAL_Q<Operation_Logger_Type> Rebuild_Logger;
-    PointVector Rebuild_PCL_Storage;
-    iKdTreeNode **Rebuild_Ptr = nullptr;
+    // std::queue<Operation> Rebuild_Logger;
+    MANUAL_Q<Operation> Rebuild_Logger;
+    _PointCloud Rebuild_PCL_Storage;
+    Node **Rebuild_Ptr = nullptr;
     int search_mutex_counter = 0;
 
     // KD Tree Functions and augmented variables
@@ -308,8 +291,8 @@ private:
     float balance_criterion_param = 0.7f;
     float downsample_size = 0.2f;
     bool Delete_Storage_Disabled = false;
-    iKdTreeNode *STATIC_ROOT_NODE = nullptr;
-    PointVector Points_deleted;
-    PointVector Downsample_Storage;
-    PointVector Multithread_Points_deleted;
+    Node *STATIC_ROOT_NODE = nullptr;
+    _PointCloud Points_deleted;
+    _PointCloud Downsample_Storage;
+    _PointCloud Multithread_Points_deleted;
 };
